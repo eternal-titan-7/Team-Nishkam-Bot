9ecdc50c9c6ce58fb15617707d488ac3c6c100f0
CoolFool <bhaweshsoni29@gmail.com>
2021-05-25 04:07:41 +0530

    =_=

--- infMain.py
+++ infMain.py
@@ -1,5 +1,5 @@
 from pyrogram import Client
-
+import tgcrypto
 import vars
 
 infBot = Client(session_name=":memory:", api_id=vars.API_ID, api_hash=vars.API_HASH, bot_token=vars.BOT_TOKEN, plugins=dict(root="cmdHandlers"))
